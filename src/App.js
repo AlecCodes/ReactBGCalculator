42f61a8d72de4bb2692b6de188bf506d21d37738
Alec Hannaford <alec322@gmail.com>
2023-08-26 16:42:56 -0700

    checkbox changes onsubmit function

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
 
   const [mmButtonState, setMMButtonState] = useState(false)
   const [mgButtonState, setMGButtonState] = useState(false)
+  const [resultsState, setResultsState] = useState('')
+  const [conversionState, setConversionState] = useState(null)
 
 
   ///////////////////////////////////
@@ -16,18 +18,31 @@ function App() {
   function mmClickHandler(){
     setMMButtonState(true)
     setMGButtonState(false)
+    setConversionState(() => mmolToMG)
   }
 
   function mgClickHandler(){
     setMGButtonState(true)
     setMMButtonState(false)
+    setConversionState(() => mgToMM)
   }
+
+  function mmolToMG(event){
+    event.preventDefault();
+    console.log("THIS BUT CONVERTED TO MG")
+  }
+
+  function mgToMM(event){
+    event.preventDefault();
+    console.log( "THIS BUT CONVERTED TO MM")
+  }
+
 
   return (
     <div className="App">
       <h1>BG Calculator</h1>
       <div className="formContainer">
-        <form>
+        <form onSubmit={conversionState}>
           MM<input type="checkbox" 
           onChange={mmClickHandler}
           checked = {mmButtonState}
@@ -40,11 +55,14 @@ function App() {
           <input type="Submit"/>
         </form>
       </div>
+
       <div className="resultsContainer">
         <div>
           <h2>Converted Value:</h2>
           <h3></h3>
-          <input type="text"></input>
+          <input type="text" 
+          value={resultsState}
+          ></input>
         </div>
       </div>
     </div>
