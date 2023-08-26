a67a96e9c9dd963176eab4a779556692546f172b
Alec Hannaford <alec322@gmail.com>
2023-08-26 16:02:31 -0700

    inital Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>BG Calculator</h1>
+      <div className="formcontainer">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
