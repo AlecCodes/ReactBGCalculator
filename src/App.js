14f9e8bf7ea80d4a60a91051d6ac47adbca3c9c8
Alec Hannaford <alec322@gmail.com>
2023-08-27 10:58:16 -0700

    bg entry state is not being used by conversion function appropriately. Im reverting

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import {useState} from 'react';
 import {getMGPerdL, getMMOLPerLiter} from './conversions'
 
+//Only after changing buttons does the submit button use the proper calcualtion. Maybe we don't use a form?
+
+
 function App() {
 
-  const [mmButtonState, setMMButtonState] = useState(false)
+  const [mmButtonState, setMMButtonState] = useState(true)
   const [mgButtonState, setMGButtonState] = useState(false)
   const [resultsState, setResultsState] = useState('')
-  const [conversionState, setConversionState] = useState(null)
+  //This should default to one or the other
+  const [conversionState, setConversionState] = useState()
+  const [BGEntryState, setBGEntryState] = useState(0)
 
 
   ///////////////////////////////////
@@ -18,31 +23,50 @@ function App() {
   function mmClickHandler(){
     setMMButtonState(true)
     setMGButtonState(false)
+    setBGEntryState(0)
     setConversionState(() => mmolToMG)
   }
 
   function mgClickHandler(){
     setMGButtonState(true)
     setMMButtonState(false)
+    setBGEntryState(0)
     setConversionState(() => mgToMM)
   }
 
   function mmolToMG(event){
-    event.preventDefault();
-    console.log("THIS BUT CONVERTED TO MG")
+    event.preventDefault()
+    console.log(getMGPerdL(BGEntryState), "SHOULD BE THE RESULT...")
+    setResultsState(getMGPerdL(BGEntryState))
   }
 
   function mgToMM(event){
-    event.preventDefault();
-    console.log( "THIS BUT CONVERTED TO MM")
+    event.preventDefault()
+    console.log(getMMOLPerLiter(BGEntryState), "SHOULD BE THE RESULT...")
+    setResultsState(getMMOLPerLiter(BGEntryState))
   }
 
+  ///////////////////////////////////
+  //CHANGE HANDLERS
+  ///////////////////////////////////
+
+  function BGInputHandler(event){
+    event.preventDefault()
+    console.log("BG INPUT CHANGED", event.target.value)
+    setBGEntryState(event.target.value)
+  }
+
+  function handleSubmit(event){
+    event.preventDefault()
+    conversionState()
+  }
+ 
 
   return (
     <div className="App">
       <h1>BG Calculator</h1>
       <div className="formContainer">
-        <form onSubmit={conversionState}>
+        <form>
           MM<input type="checkbox" 
           onChange={mmClickHandler}
           checked = {mmButtonState}
@@ -51,18 +75,25 @@ function App() {
           onChange={mgClickHandler}
           checked = {mgButtonState}
           />
-          <input type="number"/>
+          <input type="number"
+          onChange={BGInputHandler}
+          value={BGEntryState}
+          />
           <input type="Submit"/>
         </form>
       </div>
+      
+      <button
+      onClick={conversionState}
+      >
+        Try this
+      </button>
 
       <div className="resultsContainer">
         <div>
           <h2>Converted Value:</h2>
           <h3></h3>
-          <input type="text" 
-          value={resultsState}
-          ></input>
+          <h3>{resultsState}</h3>
         </div>
       </div>
     </div>
